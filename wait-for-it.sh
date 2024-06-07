7f592de771f5d8477130428047966d7f690179e9
myTech.Today <mytechtoday@protonmail.com>
2024-06-07 07:43:36 -0500

    modified:   docker-compose.yml 	modified:   wait-for-it.sh

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -6,10 +6,20 @@ set -e
 host="$1"
 shift
 cmd="$@"
+timeout=60
+start_time=$(date +%s)
 
 until mysql -h "$host" -u root -proot -e 'SELECT 1' &> /dev/null; do
   >&2 echo "MySQL is unavailable - sleeping"
   sleep 5
+
+  current_time=$(date +%s)
+  elapsed_time=$((current_time - start_time))
+  
+  if [ $elapsed_time -ge $timeout ]; then
+    >&2 echo "MySQL is still unavailable after $timeout seconds, exiting."
+    exit 1
+  fi
 done
 
 >&2 echo "MySQL is up - executing command"
